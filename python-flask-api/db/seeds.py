b29b038add81f7932ec0464497de2aea54d1d35d
aaron <alkrauss48@gmail.com>
2019-02-06 13:09:27 -0600

    Refactor seeds

--- python-flask-api/db/seeds.py
+++ python-flask-api/db/seeds.py
@@ -2,17 +2,21 @@ from peewee import *
 
 from models import Item, ItemList
 
-# Delete the table data.
-Item.delete().execute()
-ItemList.delete().execute()
+def seed():
+    # Delete the table data.
+    Item.delete().execute()
+    ItemList.delete().execute()
 
-# Create the initial item list
-item_list = ItemList(name='Main List')
-item_list.save()
+    # Create the initial item list
+    item_list = ItemList(name='Main List')
+    item_list.save()
 
-ItemList(name='Secondary List').save()
+    ItemList(name='Secondary List').save()
 
-# Create items under the item list
-Item(name='Clean the room', item_list=item_list.id).save()
-Item(name='Take out the trash', item_list=item_list.id).save()
-Item(name='Sweep the floors', item_list=item_list.id).save()
+    # Create items under the item list
+    Item(name='Clean the room', item_list=item_list.id).save()
+    Item(name='Take out the trash', item_list=item_list.id).save()
+    Item(name='Sweep the floors', item_list=item_list.id).save()
+
+if __name__ == "__main__":
+    seed()
