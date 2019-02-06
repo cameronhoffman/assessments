13a4eda1a6cb7f32b964115a537a6fd2237b9da6
aaron <alkrauss48@gmail.com>
2019-02-06 12:00:16 -0600

    Add DB config in base model TODO: Figure out why the import was failing

--- python-flask-api/models/base.py
+++ python-flask-api/models/base.py
@@ -1,6 +1,14 @@
 from peewee import *
 
-from db import db
+# from db import db
+
+db = SqliteDatabase('db/todo.db', pragmas={
+    'journal_mode': 'wal',
+    'cache_size': -1 * 64000,  # 64MB
+    'foreign_keys': 1,
+    'ignore_check_constraints': 0,
+    'synchronous': 0})
+
 
 class BaseModel(Model):
     class Meta:
