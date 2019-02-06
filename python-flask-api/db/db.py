7b9e0c0aa8efcf111cb629faa3c3c1487996f4b9
aaron <alkrauss48@gmail.com>
2019-02-06 10:49:53 -0600

    Update DB schema Create DB singleton file

---
+++ python-flask-api/db/db.py
@@ -0,0 +1,8 @@
+from peewee import *
+
+db = SqliteDatabase('db/todo.db', pragmas={
+    'journal_mode': 'wal',
+    'cache_size': -1 * 64000,  # 64MB
+    'foreign_keys': 1,
+    'ignore_check_constraints': 0,
+    'synchronous': 0})
